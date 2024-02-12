d113f80391903b71e08cf4a9b5c3781c1ddf9a5d
Christeena Mathew <christeenamathew248@gmail.com>
2024-02-12 23:46:50 +0500

    Mid term submission

---
+++ src/arithmetic/Arithmetic.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arithmetic;
+
+
+import java.util.Scanner;
+import static java.time.Clock.system;
+
+/** This class calls the method to perform 
+ * arithmetic operations based on user input
+ * execute the code check the output
+ * @author sivagamasrinivasan
+ * 
+ */
+public class Arithmetic 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       
+        ArithmeticBase r= new ArithmeticBase();
+        Scanner in= new Scanner(System.in);
+        int n= in.nextInt();
+        int m= in.nextInt();
+        for (ArithmeticOperation operation : ArithmeticOperation.values()) {
+            double result = r.calculate(n, m, operation);
+            System.out.println("Result of " + operation + ": " + result);
+        }
+    
+    }
+}
+
